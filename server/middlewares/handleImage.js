7000fcccfaff29e959c2bac9c91deb4986b97ead
Asma Rasheed <Asmarasheed4599@gmail.com>
2024-06-11 00:21:51 +0300

    handling uploading multi media

--- server/middlewares/handleImage.js
+++ server/middlewares/handleImage.js
@@ -1,13 +1,26 @@
+// middleware/upload.js
 const multer = require('multer');
+const path = require('path');
 
-const storage = multer.diskStorage({
+const imageStorage = multer.diskStorage({
   destination: 'public/images',
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    const extension = file.originalname.split('.').pop();
-    cb(null, file.fieldname + '-' + uniqueSuffix + '.' + extension);
-  },
+    const extension = path.extname(file.originalname);
+    cb(null, 'image-' + uniqueSuffix + extension);
+  }
 });
 
-exports.uploadSingle = multer({ storage: storage }).single('image');
-// exports.uploadMultiple = multer({ storage: storage }).array('images', 2)
+const videoStorage = multer.diskStorage({
+  destination: 'public/videos',
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    const extension = path.extname(file.originalname);
+    cb(null, 'video-' + uniqueSuffix + extension);
+  }
+});
+
+const uploadImage = multer({ storage: imageStorage }).single('image');
+const uploadVideo = multer({ storage: videoStorage }).single('video');
+
+module.exports = { uploadImage, uploadVideo };
--- server/middlewares/uploadMiddleware.js
+++ server/middlewares/uploadMiddleware.js
@@ -2,20 +2,25 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'public/images'); // Specify the destination folder for saving the images
+    if (file.mimetype.startsWith('image/')) {
+      cb(null, 'public/images'); // Store images in 'public/images' folder
+    } else if (file.mimetype.startsWith('video/')) {
+      cb(null, 'public/videos'); // Store videos in 'public/videos' folder
+    } else {
+      cb(new Error('Unsupported file type'));
+    }
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
     const extension = file.originalname.split('.').pop();
     const filename = file.fieldname + '-' + uniqueSuffix + '.' + extension;
-    const imagePath = 'public/images/' + filename;
     cb(null, filename);
-    req.imagePaths = req.imagePaths || []; // Store image paths in the request object
-    req.imagePaths.push(imagePath);
+    req.filePaths = req.filePaths || []; // Store file paths in the request object
+    req.filePaths.push(filename);
   },
 });
 
 exports.upload = multer({ storage: storage }).fields([
-  { name: 'image1', maxCount: 1 },
-  { name: 'image2', maxCount: 1 },
+  { name: 'image', maxCount: 1 },
+  { name: 'video', maxCount: 1 },
 ]);
