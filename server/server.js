12f3c0639871cbcb582db767cac557fa3bc68c35
Asma Rasheed <Asmarasheed4599@gmail.com>
2024-06-11 00:22:39 +0300

    using the courses router and the middlwares + adding a size limit on the uploaded files

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const dbURI = process.env.dbURI;
 const mongoose = require("mongoose");
 
 const userRouts = require("./routes/userRoute");
-
+const courseRouts = require("./routes/courseRoute");
 const notFoundHandler = require("./middlewares/404");
 
 
@@ -14,10 +14,14 @@ const Protected = require("./middlewares/Protected");
 
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 app.use(cors());
 app.use(userRouts)
+app.use(courseRouts)
+app.use(notFoundHandler);
+app.use(errorHandler);
+app.use(Protected);
 
 const path = require("path");
 app.use("/public", express.static(path.join(__dirname, "public")));
