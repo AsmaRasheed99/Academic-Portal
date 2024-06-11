3661ebc2dd1204e4bf38d54b5dfade301e9b9c66
Asma Rasheed <asmarasheed4599@gmail.com>
2024-06-11 16:38:25 +0300

    updated the navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,32 +1,76 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Button, Navbar } from "flowbite-react";
-import img from "../images/RegistrationIllustraion.png";
-const Nav = () => {
+const Nav = (props) => {
+  const role = props.UserApp.role;
+  const token = localStorage.getItem("auth");
+
+  const handleLogout = async (e) => {
+    e.preventDefault();
+     localStorage.removeItem("auth");
+     console.log("logged out");
+  }
+
   return (
     <Navbar fluid rounded className="shadow-sm">
-      <Navbar.Brand as={Link} href="https://flowbite-react.com" className="px-5">
-        <img src={img} className="mr-3 h-8 sm:h-9" alt="Flowbite React Logo" />
+      <Navbar.Brand
+        as={Link}
+        href="https://flowbite-react.com"
+        className="px-5"
+      >
         <span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
-          Flowbite React
+          Mind<span className="text-[#0e7490] text-3xl">Q</span>uest
         </span>
       </Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse className="px-5 ">
-        <Navbar.Link active className="text-lg flex  items-center h-full">
+        <Navbar.Link className="text-lg flex  items-center h-full">
           <Link to="/">Home</Link>
         </Navbar.Link>
-        <Navbar.Link className="text-lg flex  items-center h-full" as={Link} href="#">
-          About
+        <Navbar.Link className="text-lg flex  items-center h-full" href="#">
+          <Link to="/Allcourses">Courses</Link>
         </Navbar.Link>
-        <Navbar.Link className="text-lg flex  items-center h-full" href="#"><Link to='/Allcourses'>Courses</Link></Navbar.Link>
-        <Navbar.Link className="text-lg flex  items-center h-full" href="#">Pricing</Navbar.Link>
-        <Navbar.Link className="text-lg flex  items-center h-full" href="">
-          {" "}
-          <Link to='/login'>
-          <Button className="p-0 m-0 ">Log in</Button>
-          </Link>
+        {role === "teacher" ? (
+          <>
+          <Navbar.Link className="text-lg flex  items-center h-full" href="#">
+            <Link to="/AddCourse">Create New Course</Link>
+          </Navbar.Link>
+          <Navbar.Link className="text-lg flex  items-center h-full" href="#">
+            <Link to="/UpdateCourses">Courses Management</Link>
+          </Navbar.Link>
+          </>
+        ) : null}
+
+        <Navbar.Link
+          className="text-lg flex  items-center h-full"
+          as={Link}
+          href="#"
+        >
+          About us
         </Navbar.Link>
+        <Navbar.Link
+          className="text-lg flex  items-center h-full"
+          href="#"
+        ></Navbar.Link>
+        {token ? (
+          <>
+            {" "}
+            <Navbar.Link className="text-lg flex  items-center h-full" href="">
+              {" "}
+                <Button className="p-0 m-0 " onClick={(e)=>{handleLogout(e)}}>Log out</Button>
+            </Navbar.Link>
+          </>
+        ) : (
+          <>
+            {" "}
+            <Navbar.Link className="text-lg flex  items-center h-full" href="">
+              {" "}
+              <Link to="/login">
+                <Button className="p-0 m-0 ">Log in</Button>
+              </Link>
+            </Navbar.Link>
+          </>
+        )}
       </Navbar.Collapse>
     </Navbar>
   );
