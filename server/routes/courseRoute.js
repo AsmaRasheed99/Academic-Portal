795aa0c1dcb7b4527834befe9935c0f4a3090fa7
Asma Rasheed <asmarasheed4599@gmail.com>
2024-06-11 16:33:44 +0300

    added api routes to delete and updates courses

--- server/routes/courseRoute.js
+++ server/routes/courseRoute.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const courseController = require("../controllers/courseController");
-const { upload } = require("../middlewares/uploadMiddleware");
+const upload = require('../middlewares/uploadMiddleware').upload;
 
 router.post("/api/courses", upload, courseController.createCourse);
 router.get("/api/getAllCourses", courseController.getAllCourses);
 router.get("/api/getTeacherCourses/:id", courseController.getTeacherCourses);
 router.get("/api/getCourse/:id", courseController.getCourse);
-
+router.delete('/api/DeleteCourse/:id', courseController.DeleteCourse);
+router.put('/api/UpdateCourse/:id', courseController.UpdateCourse);
 module.exports = router;
